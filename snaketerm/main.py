f44c54221448379ddc409b87c2b3edea7857d0d4
busayojee <busayo_jee@yahoo.com>
2025-01-23 01:46:38 +0000

    snake game for pypi -2

--- snaketerm/main.py
+++ snaketerm/main.py
@@ -1,60 +1,20 @@
 import curses
-from game import Game
+from .game import Game
+import click
+from . import VERSION_STRING
 
-
-def main(stdscr):
+def play(stdscr):
     game = Game(stdscr)
     game.run()
-
-if __name__ == '__main__':
-    curses.wrapper(main)
-
-
-# def main(stdscr):
-#     curses.curs_set(0)  
-#     stdscr.nodelay(1)   
-#     stdscr.timeout(150) 
-#     sh, sw = stdscr.getmaxyx()
-#     game_window = curses.newwin(sh, sw, 0, 0)
-#     game_window.keypad(1)
-#     snake = Snake(0, 0, sh, sw)
-#     food = (random.randint(1, sh - 2), random.randint(1, sw - 2))
-#     game_window.addch(food[0], food[1], curses.ACS_PI) 
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(VERSION_STRING)
+    ctx.exit()
+@click.command()
+@click.option("-v", "--version", is_flag=True, callback=print_version,expose_value=False, is_eager=True, help="Show version and exit")
+def main():
+    curses.wrapper(play)
+# if __name__ == '__main__':
+#     main()
     
-#     # Game loop
-#     while True:
-#         # Handle user input
-#         key = game_window.getch()
-#         if key == curses.KEY_UP:
-#             snake.change_direction('UP')
-#         elif key == curses.KEY_DOWN:
-#             snake.change_direction('DOWN')
-#         elif key == curses.KEY_LEFT:
-#             snake.change_direction('LEFT')
-#         elif key == curses.KEY_RIGHT:
-#             snake.change_direction('RIGHT')
-#         elif key == ord('q'): 
-#             break
-        
-#         try:
-#             snake.move()
-#         except ValueError:  
-#             break
-        
-#         if (snake.head.x, snake.head.y) == food:
-#             snake.grow()
-#             food = (random.randint(1, sh - 2), random.randint(1, sw - 2))
-#             game_window.addch(food[0], food[1], curses.ACS_PI)
-    
-#         game_window.clear()
-#         game_window.addch(food[0], food[1], curses.ACS_PI)
-#         node = snake.head
-#         while node:
-#             game_window.addch(node.x, node.y, '#')
-#             node = node.next
-        
-#         game_window.refresh()
-#         time.sleep(0.1) 
-
-
-# curses.wrapper(main)
